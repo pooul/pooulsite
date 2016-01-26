9ac8ed74bd165cb5408a319848259d965faa8e27
Helapu <foolway@126.com>
2016-01-26 11:47:13 +0800

    clone

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+	def Home
+		
+	end
+
+	def qpos
+		
+	end
+
+	def press
+		
+	end
+
+	def support
+		
+	end
+
+	def  safety_and_security 
+		
+	end
+
+	def about
+		
+	end
+
+	def partner
+		
+	end
+end
---
+++ public/color_front/myjs.js
@@ -0,0 +1,34 @@
+var myjs =function  () {
+	$('#carousel-example-generic').css('max-height',$(window).height());
+	$('#carousel-example-generic').css('overflow','hidden');
+	if ($(window).width()<$(window).height()) {
+		$('.carousel-inner img').css('min-width','140%');
+		$('.carousel-inner img').css('margin-left',-$('.carousel-inner img').width()*0.14+'px');
+		$('#about-hr').css('display','none');
+	}
+	if ($('div').hasClass('pages_home')) {
+		$('#navs_home').addClass('active');
+	};
+	if ($('div').hasClass('pages_qpos')) {
+		$('#navs_qpos').addClass('active');
+	};
+	if ($('div').hasClass('pages_partner')) {
+		$('#navs_partner').addClass('active');
+	};
+	if ($('div').hasClass('pages_press')) {
+		$('#navs_press').addClass('active');
+	};
+	if ($('div').hasClass('pages_supprrt')) {
+		$('#navs_supprrt').addClass('active');
+	};
+	if ($('div').hasClass('pages_safety_and_security')) {
+		$('#navs_pages_safety_and_security').addClass('active');
+	};
+
+	$('#weixin-logo').mouseover(function(){
+		$('#weixin-img').css('display','block');
+	});
+	$('#weixin-logo').mouseout(function(){
+		$('#weixin-img').css('display','none');
+	});
+}
