20d31153824957cd553831a483547e72a77cd304
wxl <wxl@pooul.cn>
2016-02-24 18:48:19 +0800

    wxl

--- app/assets/javascripts/shared/base.js
+++ app/assets/javascripts/shared/base.js
@@ -17,35 +17,19 @@ var common =function  () {
 		min_move_y:20,
 		preventDefaultEvents:false
 	});
-	if ($(window).width()<$(window).height()) {
+	if ($(window).width()<767) {
 		$('.carousel-inner img').css('min-width','140%');
 		$('.carousel-inner img').css('margin-left',-$('.carousel-inner img').width()*0.14+'px');
-		$('#about-hr').css('display','none');
-		$('#page_item i').css('display','none');
 	}
-	if ($('div').hasClass('pages_home')) {
-		$('#navs_home').addClass('active');
-	};
-	if ($('div').hasClass('pages_qpos')) {
-		$('#navs_qpos').addClass('active');
-	};
-	if ($('div').hasClass('pages_partner')) {
-		$('#navs_partner').addClass('active');
-	};
-	if ($('div').hasClass('pages_press')) {
-		$('#navs_press').addClass('active');
-	};
-	if ($('div').hasClass('pages_support')) {
-		$('#navs_support').addClass('active');
-	};
-	if ($('div').hasClass('pages_safety_and_security')) {
-		$('#navs_safety_and_security').addClass('active');
-	};
 
+    $('.navbar-nav li a[href="'+ window.location.pathname +'"]').parents('li').addClass('active');
+  	
 	$('#weixin-logo').mouseover(function(){
 		$('#weixin-img').css('display','block');
 	});
 	$('#weixin-logo').mouseout(function(){
 		$('#weixin-img').css('display','none');
 	});
+
+	$('.has-bg').anystretch();
 }
